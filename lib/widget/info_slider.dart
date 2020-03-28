085fbf957e64c4269b75a7684921a6086e41b7e9
ivanaazuka <ivana.situmorang@amarbank.co.id>
2020-03-28 23:35:32 +0700

    change content

--- lib/widget/info_slider.dart
+++ lib/widget/info_slider.dart
@@ -7,22 +7,42 @@ import 'package:flutter_swiper/flutter_swiper.dart';
 class InfoSlider extends StatefulWidget {
   final List<InfoData> infoDataList = [
     InfoData(
-        title: "On Boarding 1",
+        title: "Sering-Sering Mencuci Tangan",
         description:
-            "1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+            "Pentingnya menjaga kebersihan tangan membuat Anda memiliki risiko rendah terjangkit berbagai penyakit."),
     InfoData(
-        title: "On Boarding 2",
+        title: "Hindari Menyentuh Area Wajah",
         description:
-            "2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+        "Sangat penting menjaga kebersihan tangan sebelum dan sesudah bersentuhan dengan benda atau bersalaman dengan orang lain."),
     InfoData(
-        title: "On Boarding 3",
+        title: "Hindari Berjabat Tangan dan Berpelukan",
         description:
-            "3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+        "Menghindari kontak kulit seperti berjabat tangan mampu mencegah penyebaran virus Corona. Tangan dan wajah bisa menjadi media penyebaran virus Corona."),
     InfoData(
-        title: "On Boarding 4",
+        title: "Jangan Berbagi Barang Pribadi",
         description:
-            "4. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
-  ];
+        "Virus Corona mampu bertahan di permukaan hingga tiga hari. Penting untuk tidak berbagi peralatan makan, sedotan, handphone, dan sisir."),
+    InfoData(
+        title: "Etika ketika Bersin dan Batuk",
+        description:
+        "Ketika Anda bersin dan batuk, tutup mulut dan hidung agar orang yang ada di sekitar tidak terpapar percikan kelenjar liur."),
+    InfoData(
+        title: "Bersihkan Perabotan di Rumah",
+        description:
+        "Bersihkan permukaan perabotan rumah yang rentan tersentuh, seperti gagang pintu, meja, furnitur, laptop, handphone, apa pun, secara teratur."),
+    InfoData(
+        title: "Jaga Jarak Sosial",
+        description:
+        "Dengan menerapkan physical distancing ketika beraktivitas di luar ruangan atau tempat umum, Anda sudah melakukan satu langkah mencegah terinfeksi virus Corona."),
+    InfoData(
+        title: "Hindari Berkumpul dalam Jumlah Banyak",
+        description:
+        "Untuk saat ini, dianjurkan lebih baik melakukan aktivitas di rumah agar pandemi virus Corona cepat berlalu."),
+    InfoData(
+        title: "Mencuci Bahan Makanan",
+        description:
+        "Rendam bahan makanan, seperti buah-buah dan sayur-sayuran menggunakan larutan hidrogen peroksida atau cuka putih yang aman untuk makanan."),
+    ];
 
   @override
   _InfoSliderState createState() => _InfoSliderState();
